a8d4d80d839a679c3b21e55cb080b7a52301c6c3
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-05-14 09:58:34 +0700

    create new project

---
+++ finalproject/resources/views/layouts/backend/data/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+      Copyright &copy; {{ Date('Y') }} <div class="bullet"></div> Admin Template By <a href="https://nauval.in/">Muhamad Nauval Azhar</a> Developed Apps By <a href="https://github.com/fikrisuheri">Fikri Suheri</a>
+    </div>
+    <div class="footer-right">
+      2.3.0
+    </div>
+  </footer>
