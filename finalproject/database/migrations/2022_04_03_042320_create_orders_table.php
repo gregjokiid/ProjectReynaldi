8e4944c642f5b513679b22e5f2510ad9fcd22b43
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-05-27 16:17:29 +0700

    delete unused file

--- finalproject/database/migrations/2022_04_03_042320_create_orders_table.php
+++ finalproject/database/migrations/2022_04_03_042320_create_orders_table.php
@@ -19,6 +19,18 @@ class CreateOrdersTable extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->bigInteger('total_pay');
             $table->integer('status',0)->comment('0 = Menunggu Pembayaran','1 = Mengkonfirmasi Pembayaran','2 = Pembayaran Selesai','3 = Pesanan Selesai','4 = Pesanan Dibatalkan, 5 = Pesanan Kadaluarsa');
+            $table->timestamp('paid_at')->nullable();
+            $table->string('recipient_name');
+            $table->string('proof')->nullable();
+            $table->string('destination');
+            $table->string('address_detail');
+            $table->string('courier');
+            $table->string('subtotal');
+            $table->string('shipping_cost');
+            $table->string('shipping_method');
+            $table->string('receipt_number')->nullable();
+            $table->string('total_weight');
+            $table->string('phone_number')->nullable();
             $table->timestamps();
         });
     }
