e1581bf02e4f27cf392948987182cc23eb7782e2
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-05-28 11:07:23 +0700

    update user

--- finalproject/app/Http/Controllers/UserController.php
+++ finalproject/app/Http/Controllers/UserController.php
@@ -30,15 +30,16 @@ class UserController extends Controller
 
     public function store(UserRequest $request)
     {
-        $posts = DB::table('model_has_roles')->insert([
-            'role_id' => 1,
-            'model_type' => 'App\Models\User',
-            'model_id' => 2
-            ]);
+        $user_count = count(User::all()) + 1;
 
         $data = $request->except('_token');
         $data['password'] = bcrypt($request->password);
         $this->user->store($data);
+        DB::table('model_has_roles')->insert([
+            'role_id' => $data['role'],
+            'model_type' => 'App\Models\User',
+            'model_id' => $user_count,
+        ]);
         return redirect()->route('user.index')->with('success',__('message.store'));
     }
 }
--- finalproject/resources/views/user/create.blade.php
+++ finalproject/resources/views/user/create.blade.php
@@ -9,19 +9,22 @@
                 @slot('content')
                     <x-forms.input name="name" label="Nama Lengkap" :isRequired="true" hintText="Harus sesuai dengan KTP" />
 
-                    <x-forms.select name="gender" label="Jenis Kelamin" :isRequired="true">
-                        <option value="1">Laki-laki</option>
-                        <option value="2">Perempuan</option>
+                    <x-forms.select name="role" label="Role" :isRequired="true">
+                        <option value="1">admin</option>
+                        <option value="2">user</option>
+                        <option value="3">purchasing</option>
+                        <option value="4">cashier</option>
+                        <option value="5">owner</option>
                     </x-forms.select>
 
                     <x-forms.input name="email" type="email" label="Email" :isRequired="true" />
+
                     <x-forms.input name="password" type="password" label="password" :isRequired="true" />
-                    
+
                     <div class="text-right">
                         <a href="{{ Route('user.index') }}" class="btn btn-secondary " href="#">{{ __('button.cancel') }}</a>
                         <button type="submit" class="btn btn-primary " href="#">{{ __('button.save') }}</button>
                     </div>
-
                 @endslot
             @endcomponent
         @endsection
