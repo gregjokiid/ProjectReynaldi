1035e28c7aacc62e48734b12b294ef7ee155bf01
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-23 01:26:37 +0700

    Update contact.blade.php

--- finalproject/resources/views/frontend/contact.blade.php
+++ finalproject/resources/views/frontend/contact.blade.php
@@ -6,8 +6,8 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="breadcrumb__links">
-                        <a href="{{ url('/') }}"><i class="fa fa-home"></i> Home</a>
-                        <span>Contact</span>
+                        <a href="{{ url('/') }}"><i class="fa fa-home"></i> Beranda</a>
+                        <span>Kontak</span>
                     </div>
                 </div>
             </div>
