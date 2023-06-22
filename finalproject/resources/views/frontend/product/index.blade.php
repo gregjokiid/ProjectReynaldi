68cb3fecfd2867f385f05b81fc52cae37f5b799c
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-23 01:23:05 +0700

    update product

--- finalproject/resources/views/frontend/product/index.blade.php
+++ finalproject/resources/views/frontend/product/index.blade.php
@@ -6,8 +6,8 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="breadcrumb__links">
-                        <a href="./index.html"><i class="fa fa-home"></i> Home</a>
-                        <span>Shop</span>
+                        <a href="./index.html"><i class="fa fa-home"></i> Beranda</a>
+                        <span>Produk</span>
                     </div>
                 </div>
             </div>
--- finalproject/resources/views/frontend/product/search.blade.php
+++ finalproject/resources/views/frontend/product/search.blade.php
@@ -6,8 +6,8 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="breadcrumb__links">
-                        <a href="./index.html"><i class="fa fa-home"></i> Home</a>
-                        <span>Search</span>
+                        <a href="./index.html"><i class="fa fa-home"></i> Beranda</a>
+                        <span>Pencarian</span>
                     </div>
                 </div>
             </div>
