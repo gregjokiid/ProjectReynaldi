443cb39ee1f004756617850403c23a434124959a
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-28 02:02:34 +0700

    Update navbar.blade.php

--- finalproject/resources/views/layouts/backend/data/navbar.blade.php
+++ finalproject/resources/views/layouts/backend/data/navbar.blade.php
@@ -18,7 +18,7 @@
                 @endphp
                 <div class="dropdown-list-content dropdown-list-icons">
                     @foreach($products as $product)
-                        <a href="#" class="dropdown-item dropdown-item-unread">
+                        <a href="{{ route('master.product.show', ['id' => $product->id]) }}" class="dropdown-item dropdown-item-unread">
                             <img class="dropdown-item-icon bg-primary text-white" src="{{ asset('storage/' . $product->thumbnails) }}">
                             <div class="dropdown-item-desc">
                                 {{ $product->name }}
