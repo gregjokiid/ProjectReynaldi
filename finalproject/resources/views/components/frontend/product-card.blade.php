0979eba50771a46eeee4914da0f27475888ac87c
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-29 00:05:01 +0700

    Update product-card.blade.php

--- finalproject/resources/views/components/frontend/product-card.blade.php
+++ finalproject/resources/views/components/frontend/product-card.blade.php
@@ -1,17 +1,14 @@
 <div class="product__item">
-    <div class="product__item__pic set-bg"
-        data-setbg="{{ $image }}">
-        @if(empty($stock))
-            <div class="label stockout">Habis</div>
-        @else
-            <div class="label new">Baru</div>
-        @endif
-        <ul class="product__hover">
-            <li><a href="{{ $image }}"
-                    class="image-popup"><span class="arrow_expand"></span></a></li>
-            <li><a href="{{ $route }}"><span><i class="fa fa-eye"></i></span></a></li>
-        </ul>
-    </div>
+    <a href="{{ $route }}">
+        <div class="product__item__pic set-bg"
+             data-setbg="{{ $image }}">
+            @if(empty($stock))
+                <div class="label stockout">Habis</div>
+            @else
+                <div class="label new">Baru</div>
+            @endif
+        </div>
+    </a>
     <div class="product__item__text">
         <h6><a href="{{ $route }}">{{ $name }}</a></h6>
         <div class="rating">
@@ -24,3 +21,9 @@
         <div class="product__price">{{ rupiah($price) }}</div>
     </div>
 </div>
+
+{{--<ul class="product__hover">--}}
+{{--    <li><a href="{{ $image }}"--}}
+{{--           class="image-popup"><span class="arrow_expand"></span></a></li>--}}
+{{--    <li><a href="{{ $route }}"><span><i class="fa fa-eye"></i></span></a></li>--}}
+{{--</ul>--}}
