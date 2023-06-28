dd4ac7bda6d3191d113aa985e6822600576b3b8f
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-06-28 13:38:03 +0700

    Update app.blade.php

--- finalproject/resources/views/layouts/frontend/app.blade.php
+++ finalproject/resources/views/layouts/frontend/app.blade.php
@@ -11,6 +11,8 @@
     @yield('content')
     @include('layouts.frontend.data.footer')
 
+    <a href="#"><img src="upbutton.png" id="fixedbutton"></a>
+
     <!-- Search Begin -->
     <div class="search-model">
         <div class="h-100 d-flex align-items-center justify-content-center">
