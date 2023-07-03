87d56fc8a9bddb565af16abcd15f9e837a786b90
Franciscus Valentinus Ongkosianbhadra <franciscusvalentinus@gmail.com>
2023-07-03 13:08:39 +0700

    Update invoice.blade.php

--- finalproject/resources/views/email/invoice.blade.php
+++ finalproject/resources/views/email/invoice.blade.php
@@ -124,6 +124,8 @@
             <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 600px;">
                 <tr>
                     <td align="left" bgcolor="#ffffff" style="padding: 36px 24px 0; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; border-top: 3px solid #d4dadf;">
+                        <center><image src="{{ asset('ashion') }}/img/logo-no-background.png" width="50%"></image></center>
+                        <br>
                         <h1 style="margin: 0; font-size: 32px; font-weight: 700; letter-spacing: -1px; line-height: 48px;">Invoice {{ $data['order']->invoice_number }}</h1>
                     </td>
                 </tr>
